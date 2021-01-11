0a0c04f859d34214d391d9f225241e2d0bd33057
Shan <srikishan2000@gmail.com>
2021-01-11 21:34:45 +0200

    React spring project skeleton

---
+++ ReactAndSpringDataRest/src/main/java/com/example/springdatareact/SpringdatareactApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatareact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatareactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatareactApplication.class, args);
+	}
+
+}
---
+++ ReactAndSpringDataRest/src/test/java/com/example/springdatareact/SpringdatareactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatareact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatareactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
